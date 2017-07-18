b3e4a13805cae16438d4c912a48c86c7798bac3f
weisw.shi <weisw.shi@qunar.com>
2017-07-18 15:23:28 +0800

    313`1

--- src/main/java/com/me/sweet/TestApplication.java
+++ src/main/java/com/me/sweet/TestApplication.java
@@ -2,13 +2,22 @@ package com.me.sweet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController("com.me.sweet.web")
-public class TestApplication {
+public class TestApplication  extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+	}
+
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8081);
 	}
 }
