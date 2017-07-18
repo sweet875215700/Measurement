4a7278dc775a6d430a8bde412e9bdd1aaade8f5b
weisw.shi <weisw.shi@qunar.com>
2017-07-18 15:46:54 +0800

    313`1

---
+++ src/main/java/com/me/sweet/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.me.sweet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by weisw.shi on 2017/7/18.
+ */
+public class ServletInitializer extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(TestApplication.class);
+    }
+}
--- src/main/java/com/me/sweet/TestApplication.java
+++ src/main/java/com/me/sweet/TestApplication.java
@@ -1,14 +1,18 @@
 package com.me.sweet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController("com.me.sweet.web")
+@EnableAutoConfiguration
+@ComponentScan
 public class TestApplication  extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
 	public static void main(String[] args) {
